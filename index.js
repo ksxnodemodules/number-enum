f5eddfeba86c9fa4bc2509d2d576972d778a0ec8
khai96_ <hvksmr1996@gmail.com>
2018-01-03 13:40:50 +0700

    Create function 'create'

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict'
+const ParallelIterable = require('parallel-iterable')
+const {freeze, assign} = Object
 
 function * generate (current = 0, step = 1) {
   yield current
   yield * generate(current + step, step)
 }
 
-module.exports = {
-  generate
+function create (names = [], current = 0, step = 1) {
+  return freeze(
+    new ParallelIterable(ParallelIterable.END_OF_FIRST, names, generate(current, step))
+      .reduce((prev, [key, val]) => assign(prev, {[key]: val}), {})
+  )
 }
+
+module.exports = assign(create, {
+  generate,
+  create
+})
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,40 @@
 'use strict'
 const {ProductIterable, RangeIterable, ConcatIterable} = require('x-iterable')
 const main = require('..')
-const {generate} = main
+const {generate, create} = main
+
+const represent = x => {
+  if (!x) return String(x)
+
+  switch (typeof x) {
+    case 'string':
+    case 'number':
+    case 'boolean':
+      return JSON.stringify(x)
+    case 'object':
+      return x instanceof Array
+        ? `[${
+          x.map(represent).join(', ')
+        }]`
+        : `{${
+          Object
+            .entries(x)
+            .filter(([key]) => typeof key === 'string')
+            .map(([key, val]) => key + ': ' + represent(val))
+            .join(', ')
+        }}`
+    default:
+      throw new TypeError('Cannot represent this type of value')
+  }
+}
 
 describe('Basic assertions', () => {
   describe('Types', () => {
     test('generate should be a function', () => expect(typeof generate).toBe('function'))
+    test('create should be a function', () => expect(typeof create).toBe('function'))
   })
+
+  test('create.create === create', () => expect(main).toBe(create))
 })
 
 describe('generate(begin, step)', () => {
@@ -36,4 +64,37 @@ describe('generate(begin, step)', () => {
       actualBegin + actualStep * 3
     ]))
   }))
+})
+
+describe('create(names, begin, step)', () => {
+  const typeCheckObject = object => Object.entries(object).forEach(
+    ([key, val]) =>
+      test(`val = ${val}, key = ${key}`, () => expect(typeof val).toBe('number'))
+  )
+
+  new ProductIterable(
+    [undefined, [], 'abc', ['def', 'ghi', 'jkl']],
+    ...ConcatIterable.times(
+      [new ConcatIterable(
+        [undefined],
+        RangeIterable
+          .range(-10, 10)
+          .map(x => x / 4)
+      )],
+      2
+    )
+  ).forEach(([names, begin, step]) => {
+    const result = create(names, begin, step)
+    const actualNames = names || []
+
+    describe('types', () => typeCheckObject(result))
+
+    describe(
+      `names = ${represent(names)}, begin = ${begin}, step = ${step}, result = ${represent(result)}`,
+      () => {
+        const keys = Object.keys(result)
+        expect(keys.length).toBe(actualNames.length)
+      }
+    )
+  })
 })
