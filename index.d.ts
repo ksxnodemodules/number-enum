b8684c9ee0c4230b60298a3015aa4b4aec7c8bca
khai96_ <hvksmr1996@gmail.com>
2018-01-13 21:07:22 +0700

    Improve TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,14 @@
-type KEY = string;
+type KEY = string | number | Symbol;
 type VAL = number;
+type SEQ<X> = Iterator<X> | Iterable<X>;
 
 declare function GENERATE(current?: VAL, step?: VAL): Iterator<VAL>;
-declare function MAIN(names: KEY[], current?: VAL, step?: VAL): { [key: string] : VAL; };
+declare function MAIN(names: SEQ<KEY>, current?: VAL, step?: VAL): { [key: string] : VAL; };
 
 declare namespace MAIN {
   export type Key = KEY;
   export type Val = VAL;
+  export type Seq<X> = SEQ<X>;
   export const generate: typeof GENERATE;
   export const create: typeof MAIN;
 }
